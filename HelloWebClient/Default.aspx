3061b597dc69018a6a144bcff9e8ccc8e28f2ca4
Mohsin Khan <mokha@microsoft.com>
2020-08-22 12:47:37 +0530

    Add project files.

---
+++ HelloWebClient/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="HelloWebClient._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+   
+
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click1" Text="Button" />
+    <br />
+    <asp:Label ID="Label1" runat="server" ></asp:Label>
+
+</asp:Content>
---
+++ HelloWebClient/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using HelloWebClient.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HelloWebClient
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+        
+     
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            HelloService1Client client = new HelloService1Client("BasicHttpBinding_IHelloService1");
+            var message = client.GetMessage(TextBox1.Text);
+            Label1.Text = message;
+        }
+    }
+}
